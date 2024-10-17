ee0861d8a86714764f6bb1609030ee51aeafee4f
Isaac-Amano <yishakamano2020@gmail.com>
2024-10-16 20:20:19 -0500

    set up project, installed dependencies

---
+++ server/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: 'dqmczrixn',
+    api_key: '389619846968422',
+    api_secret: 'AxtZvpPCVQJ2EnfPHUqEwydcyJE'
+  });
+  
+module.exports = cloudinary;
